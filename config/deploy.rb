8254a80458a1d1cb900309df306ae4f99d2fbd95
KristianAlves <kristian.alves@gmail.com>
2017-04-20 22:15:18 +0000

    Instalando o Capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :keep_releases, 5
 # Default value for :format is :airbrussh.
 set :format, :airbrussh
 set :log_level, :debug
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
