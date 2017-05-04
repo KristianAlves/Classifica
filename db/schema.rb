c229e00cee4a84e1fc6f29b54d4caba0df40668e
KristianAlves <kristian.alves@gmail.com>
2017-05-04 23:59:45 +0000

    Criação do Carrinho de Compras.

--- db/schema.rb
+++ db/schema.rb
@@ -129,17 +129,6 @@ ActiveRecord::Schema.define(version: 20170501180206) do
   add_index "members", ["email"], name: "index_members_on_email", unique: true
   add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
 
-  create_table "orders", force: :cascade do |t|
-    t.integer  "ad_id"
-    t.integer  "status",     default: 0
-    t.integer  "buyer_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  add_index "orders", ["ad_id"], name: "index_orders_on_ad_id"
-  add_index "orders", ["buyer_id"], name: "index_orders_on_buyer_id"
-
   create_table "overall_averages", force: :cascade do |t|
     t.integer  "rateable_id"
     t.string   "rateable_type"
