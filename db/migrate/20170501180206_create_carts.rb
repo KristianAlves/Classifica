82a2a3b973f1681ab1f4b1fbc0dfd8f64a4ce39e
KristianAlves <kristian.alves@gmail.com>
2017-05-06 19:15:31 +0000

    Criação do Carrinho de Compras.

--- db/migrate/20170501180206_create_carts.rb
+++ db/migrate/20170501180206_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
       t.references :ad, index: true, foreign_key: true
-      t.references :buyer, index: true, foreign_key: true
+      t.references :buyer, index: true, foreign_key: { to_table: :members }
       t.references :description, index: true, foreign_key: true
       t.float :amount
       t.integer :quantity, default: 1
